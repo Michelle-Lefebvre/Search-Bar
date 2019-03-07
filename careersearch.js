cd5997b19636e6a05791ebcfa29d54aabc140225
Michelle Lefebvre <mmldesigns007@gmail.com>
2019-03-07 09:05:16 -0500

    Initial commit

---
+++ careersearch.js
@@ -0,0 +1,387 @@
+var dataset = {
+  Arizona: {
+    Tempe: {
+      'NE Phoenix': 'https://nephoenix.madscience.org/workforus.aspx'
+    }
+  },
+
+  California: {
+    Concord: {
+      'Mount Diablo & North Bay': 'https://mtdiablo.madscience.org/workforus.aspx'
+    },
+
+    Fremont: {
+      'The Bay Area': 'https://thebayarea.madscience.org/workforus.aspx'
+    },
+
+    Brea: {
+      'North Orange County': 'https://northorange.madscience.org/workforus.aspx'
+    },
+
+    Laguna_Hills: {
+      'South Orange County': 'https://soorangecounty.madscience.org/workforus.aspx'
+    },
+
+    Santa_Ana: {
+      'West Orange County': 'https://westorangecounty.madscience.org/workforus.aspx'
+    },
+
+    Sacramento: {
+      'Sacramento Valley': 'https://sacramento.madscience.org/workforus.aspx'
+    },
+
+    San_Diego: {
+      'San Diego': 'https://sandiego.madscience.org/workforus.aspx'
+    },
+
+    Sylmar: {
+      'Los Angeles': 'https://losangeles.madscience.org/workforus.aspx'
+    }
+  },
+
+  Colorado: {
+    Denver: {
+      'Colorado': 'https://colorado.madscience.org/workforus.aspx'
+    }
+  },
+
+  Connecticut: {
+    Stratford: {
+      'Fairfield County': 'https://connecticut.madscience.org/workforus.aspx'
+    }
+  },
+
+  Delaware: {
+    West_Chester: {
+      'Delaware Valley': 'https://delawarevalley.madscience.org/workforus.aspx'
+    }
+  },
+
+  Florida: {
+    St_Petersburg: {
+      'Greater Tampa Bay': 'https://greatertampabay.madscience.org/workforus.aspx'
+    },
+    Oviedo: {
+      'N.E. Central Florida': 'https://cfl.madscience.org/workforus.aspx'
+    },
+    Jupiter: {
+      'Palm Beach': 'https://palmbeachbroward.madscience.org/workforus.aspx'
+    }
+  },
+
+  Illinois: {
+    Chicago: {
+      'Chicago': 'https://chicago.madscience.org/workforus.aspx'
+    },
+    Chicago_West: {
+      'Chicagos Western Suburbs': 'https://chicagowest.madscience.org/workforus.aspx'
+    },
+    Prospect_Heights: {
+      'Northern Illinois': 'https://northillinois.madscience.org/workforus.aspx'
+    }
+  },
+
+  Kansas: {
+    Lenexa: {
+      'Greater Kansas City': 'https://kansascity.madscience.org/workforus.aspx'
+    }
+  },
+
+  Kentucky: {
+    Louisville: {
+      'Kentucky': 'https://kentucky.madscience.org/workforus.aspx'
+    }
+  },
+
+  Louisiana: {
+    Metairie: {
+      'S.E. Louisiana': 'https://sela.madscience.org/workforus.aspx'
+    }
+  },
+
+  Maine: {
+    South_Portland: {
+      'Maine': 'https://maine.madscience.org/work-for-mad-science-of-maine.aspx'
+    }
+  },
+
+  Maryland: {
+    Silver_Spring: {
+      'Washington': 'https://dc.madscience.org/workforus.aspx'
+    }
+  },
+
+  Massachusetts: {
+    Waltham: {
+      'North Boston': 'https://northboston.madscience.org/workforus.aspx'
+    },
+    Stoughton: {
+      'South Boston': '' //missing website address
+    },
+    Fall_River: {
+      'Southern MA & RI': 'https://southernmass.madscience.org/workforus.aspx'
+    },
+    Indian_Orchard: {
+      'Western New England': 'https://westnewengland.madscience.org/workforus.aspx'
+    }
+  },
+
+  Michigan: {
+    Livonia: {
+      'Detroit': 'https://detroit.madscience.org/workforus.aspx'
+    }
+  },
+
+  Minnesota: {
+    St_Paul: {
+      'Minnesota': 'https://mn.madscience.org/workforus.aspx'
+    }
+  },
+
+  Missouri: {
+    St_Louis: {
+      'St. Louis': 'https://stlouis.madscience.org/workforus.aspx'
+    }
+  },
+
+  New_Jersey: {
+    Denville: {
+      'Morris & Sussex': 'https://morrissussex.madscience.org/workforus.aspx'
+    },
+    Fair_Lawn: {
+      'Northeast NJ': 'https://northeastnj.madscience.org/workforus.aspx'
+    },
+    Hoboken: {
+      'Union & Hudson Counties': 'https://unionhudson.madscience.org/workforus.aspx'
+    },
+    Pennington: {
+      'West & Central New Jersey': 'https://wnj.madscience.org/workforus.aspx'
+    }
+  },
+
+  New_York: {
+    Saratoga_Springs: {
+      'Capital District': 'https://thecapitaldistrict.madscience.org/workforus.aspx'
+    },
+    East_Meadow: {
+      'Long Island': 'https://longisland.madscience.org/workforus.aspx'
+    },
+    Goshen: {
+      'Mid-Hudson': 'https://midhudson.madscience.org/workforus.aspx'
+    },
+    White_Plains: {
+      'New York City & Westchester': 'https://newyorkcity.madscience.org/workforus.aspx'
+    },
+    Rochester: {
+      'Western New York': 'https://wny.madscience.org/workforus.aspx'
+    }
+  },
+
+  North_Carolina: {
+    Winston_Salem: {
+      'Central Carolina': 'https://centralcarolina.madscience.org/workforus.aspx'
+    },
+    Cary: {
+      'Raleigh, Durham, Chapel Hill': 'https://triangle.madscience.org/work-for-us.aspx'
+    }
+  },
+
+  Ohio: {
+    North_Canton: {
+      'Northeast Ohio': 'https://northeastohio.madscience.org/workforus.aspx'
+    }
+  },
+
+  Oklahoma: {
+    Edmond: {
+      'Central Oklahoma': 'https://okc.madscience.org/workforus.aspx'
+    },
+  },
+
+  Oregon: {
+    Portland: {
+      'Portland & Vancouver': 'https://portland.madscience.org/workforus.aspx'
+    },
+  },
+
+  Pennsylvania: {
+    Easton: {
+      'Lehigh Valley': 'https://lehighvalley.madscience.org/workforus.aspx'
+    },
+    Cheswick: {
+      'Pittsburgh': 'https://pittsburgh.madscience.org/workforus.aspx'
+    }
+  },
+
+  South_Carolina: {
+    Garden_City: {
+      'Myrtle Beach': 'https://myrtlebeach.madscience.org/workforus.aspx'
+    }
+  },
+
+  Texas: {
+    Austin: {
+      'Austin & San Antonio': 'https://austin.madscience.org/workforus.aspx'
+    },
+    Farmers_Branch: {
+      'Dallas & Fort Worth': 'https://dfw.madscience.org/workforus.aspx'
+    },
+    Houston: {
+      'Houston': 'https://houston.madscience.org/workforus.aspx'
+    }
+  },
+
+  Utah: {
+    Farmington: {
+      'Greater Salt Lake': 'https://greatersaltlake.madscience.org/workforus.aspx'
+    }
+  },
+
+  Virginia: {
+    Chesapeake: {
+      'Hampton Roads': 'https://hamptonroads.madscience.org/workforus.aspx'
+    }
+  },
+
+  Washington: {
+    Bellevue: {
+      'Sno-King': 'https://snoking.madscience.org/workforus.aspx'
+    }
+  },
+
+  Wisconsin: {
+    Franklin: {
+      'Milwaukee': 'https://milwaukee.madscience.org/workforus.aspx'
+    }
+  }
+
+}
+
+
+// This is the state of the application when you select a new state 
+var appSelectionState = {
+  state: " ",
+  city: " ",
+  franchise: " "
+}
+
+
+
+
+function openInNewTab(event) {
+  event.preventDefault()
+  var url = dataset[appSelectionState.state][appSelectionState.city][appSelectionState.franchise]
+  if (url && url !== "") {
+    window.open(url)
+  }
+}
+
+
+function setStates(event) {
+  var selectedStateValue = event.target.value
+  if (selectedStateValue !== "") {
+    appSelectionState.state = selectedStateValue
+    var arrayOfCity = Object.keys(dataset[selectedStateValue])
+    appSelectionState.city = arrayOfCity[0]
+    var franchiseObject = dataset[selectedStateValue][arrayOfCity[0]]
+    var arrayOfFranchise = Object.keys(franchiseObject)
+    appSelectionState.franchise = arrayOfFranchise[0]
+
+
+    // populating city node here....
+    var cityNode = document.getElementById('city');
+    var length = cityNode.options.length;
+    for (i = length - 1; i >= 0; i--) {
+      cityNode.remove(i);
+    }
+
+    for (var i = 0; i < arrayOfCity.length; i++) {
+      var option = document.createElement("option");
+      option.text = arrayOfCity[i]
+      option.value = arrayOfCity[i]
+      cityNode.add(option);
+    }
+
+
+
+    // populating franchise node here ......
+    var franchiseNode = document.getElementById("franchise")
+    length = franchiseNode.options.length;
+    for (i = length - 1; i >= 0; i--) {
+      franchiseNode.remove(i);
+    }
+
+    for (var i = 0; i < arrayOfFranchise.length; i++) {
+      var option = document.createElement("option");
+      option.text = arrayOfFranchise[i]
+      option.value = arrayOfFranchise[i]
+      franchiseNode.add(option);
+    }
+
+
+  }
+
+}
+
+function setFranchise(event) {
+  var selectedFranchiseValue = event.target.value
+  console.log(selectedStateValue)
+  appSelectionState.franchise = selectedStateValue
+}
+
+function setCities() {
+  let selectedValue = event.target.value
+  appSelectionState.city = selectedValue
+
+  var franchiseObject = dataset[appSelectionState.state][selectedValue]
+  var arrayOfFranchise = Object.keys(franchiseObject)
+  appSelectionState.franchise = arrayOfFranchise[0]
+  
+  // populating franchise node here ......
+  var franchiseNode = document.getElementById("franchise")
+  length = franchiseNode.options.length;
+  for (i = length - 1; i >= 0; i--) {
+    franchiseNode.remove(i);
+  }
+
+  for (var i = 0; i < arrayOfFranchise.length; i++) {
+    var option = document.createElement("option");
+    option.text = arrayOfFranchise[i]
+    option.value = arrayOfFranchise[i]
+    franchiseNode.add(option);
+  }
+
+}
+
+function setFranchise(event) {
+  let selectedValue = event.target.value
+  console.log(selectedValue)
+  appSelectionState.franchise = selectedValue
+}
+
+function setFranchiseGo() {
+  statesSel = document.getElementById('states');
+  franchiseSel = document.getElementById('franchise');
+  franchiseGoList = franchiseGo[statesSel.value][stateSel.value][citySel.value][franchiseSel.value];
+  changeSelect('franchiseGo', franchiseGoList, franchiseGoList);
+}
+
+function setFranchise(event) {
+  let selectedValue = event.target.value
+  console.log(selectedValue)
+  appSelectionState.franchise = selectedValue
+}
+
+
+window.onload = function () {
+  var arrayOfStates = Object.keys(dataset) // This returns an array of the keys in the dataset
+  var statesNode = document.getElementById('states')
+  appSelectionState.state = arrayOfStates[0]
+  for (var i = 0; i < arrayOfStates.length; i++) {
+    var option = document.createElement("option");
+    option.text = arrayOfStates[i]
+    option.value = arrayOfStates[i]
+    statesNode.add(option);
+
+  }
+}
